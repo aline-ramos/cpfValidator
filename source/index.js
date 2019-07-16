c73dd2da27953e93e2ac5726e9b30bddf33c81c7
aline-ramos <alineramos.costa@gmail.com>
2019-07-16 14:39:38 -0300

    ajuste eslint

--- source/index.js
+++ source/index.js
@@ -1,23 +1,19 @@
 function cpfValidator(cpf) {
-	numCpf = 0;
-	resto = 0;
-
-	numCpf = 0;
-	for (let i = 1; i <= 9; i++) {
-		numCpf += parseInt(cpf.substring(i - 1, i)) * (11 - i);
-	}
-	resto = (numCpf * 10) % 11;
-
-	if (resto !== parseInt(cpf.substring(9, 10))) {
-		return false;
-	}
-	numCpf = 0;
-	for (let i = 1; i <= 10; i++) {
-		numCpf += parseInt(cpf.substring(i - 1, i)) * (12 - i);
-	}
-	resto = (numCpf * 10) % 11;
-
-	return resto === parseInt(cpf.substring(10, 11));
-
+  numCpf = 0;
+  resto = 0;
+  numCpf = 0;
+  for (let i = 1; i <= 9; i++) {
+    numCpf += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+  }
+  resto = (numCpf * 10) % 11;
+  if (resto !== parseInt(cpf.substring(9, 10))) {
+    return false;
+  }
+  numCpf = 0;
+  for (let i = 1; i <= 10; i++) {
+    numCpf += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+  }
+  resto = (numCpf * 10) % 11;
+  return resto === parseInt(cpf.substring(10, 11));
 }
 module.exports.cpfValidator = cpfValidator;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,47 +1,41 @@
-let chai = require("chai");
+let chai = require('chai');
 let expect = chai.expect;
-let cpf = require("../source/index");
-
-describe("cpfValidator", () => {
-
-    it("CPF Inválido!", () => {
-        expect(cpf.cpfValidator("011.441.953-14")).to.equal(false);
-    });
-    it("CPF Válido!", () => {
-        expect(cpf.cpfValidator("01144195314")).to.equal(true);
-    });
-    it("CPF Inválido!", () => {
-        expect(cpf.cpfValidator("01144195312")).to.equal(false);
-    });
-    it("Ops! Quantidade de número de CPF menor que o esperado!", () => {
-        expect(cpf.cpfValidator("011441953")).to.equal(false);
-    });
-    it("Ops! CPF não pode ser vazio!", () => {
-        expect(cpf.cpfValidator(" ")).to.equal(false);
-    });
-    it("CPF Inválido!", () => {
-        expect(cpf.cpfValidator("876.120.720-92")).to.equal(false);
-    });
-    it("Ops! Sinto muito! Não é um número!", () => {
-        expect(cpf.cpfValidator("aline")).to.equal(false);
-    });
-    it("CPF Válido!", () => {
-        expect(cpf.cpfValidator("65554377076")).to.equal(true);
-    });
-    it("CPF Válido!", () => {
-        expect(cpf.cpfValidator("84994584087")).to.equal(true);
-    });
-    it("Ops! Não contamos espaços!", () => {
-        expect(cpf.cpfValidator("900 815 320 32")).to.equal(false);
-    });
-    it("Ops! Sinto muito! Não é um número!", () => {
-        expect(cpf.cpfValidator("Laboratoria")).to.equal(false);
-    });
-     it("Ops! Sinto muito! Não é um número!", () => {
-        expect(cpf.cpfValidator("Laboratoria")).to.equal(false);
-    });
-      
+let cpf = require('../source/index');
+describe('cpfValidator', () => {
+  it('CPF Inválido!', () => {
+    expect(cpf.cpfValidator('011.441.953-14')).to.equal(false);
+  });
+  it('CPF Válido!', () => {
+    expect(cpf.cpfValidator('01144195314')).to.equal(true);
+  });
+  it('CPF Inválido!', () => {
+    expect(cpf.cpfValidator('01144195312')).to.equal(false);
+  });
+  it('Ops! Quantidade de número de CPF menor que o esperado!', () => {
+    expect(cpf.cpfValidator('011441953')).to.equal(false);
+  });
+  it('Ops! CPF não pode ser vazio!', () => {
+    expect(cpf.cpfValidator(' ')).to.equal(false);
+  });
+  it('CPF Inválido!', () => {
+    expect(cpf.cpfValidator('876.120.720-92')).to.equal(false);
+  });
+  it('Ops! Sinto muito! Não é um número!', () => {
+    expect(cpf.cpfValidator('aline')).to.equal(false);
+  });
+  it('CPF Válido!', () => {
+    expect(cpf.cpfValidator('65554377076')).to.equal(true);
+  });
+  it('CPF Válido!', () => {
+    expect(cpf.cpfValidator('84994584087')).to.equal(true);
+  });
+  it('Ops! Não contamos espaços!', () => {
+    expect(cpf.cpfValidator('900 815 320 32')).to.equal(false);
+  });
+  it('Ops! Sinto muito! Não é um número!', () => {
+    expect(cpf.cpfValidator('Laboratoria')).to.equal(false);
+  });
+  it('Ops! Sinto muito! Não é um número!', () => {
+    expect(cpf.cpfValidator('Laboratoria')).to.equal(false);
+  });
 });
-
-
-
